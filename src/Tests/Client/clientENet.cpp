715af3218f9de179362fda06a35172714c53f665
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2026-02-16 14:54:50 +0100

    Unwrapped enet test

--- src/Tests/Client/clientENet.cpp
+++ src/Tests/Client/clientENet.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <LittleNetwork/Serialization/Serialization.hpp>
 #include <LittleNetwork/ENet/ENetContext.hpp>
-
+#include <enet6/enet.h>
 
 int main(int argc, char** argv) {
 
@@ -14,37 +14,91 @@ int main(int argc, char** argv) {
     {
         LN::ENetContext enetContext;
 
-        
-        bool isRunning = true;
-        while (isRunning)
-        {
-            if (_kbhit())
-            {
-                char c = _getch(); 
-                if (c == '\b')
-                {
-                    isRunning = false;
-                    continue;
-                }
-                else if (c == '\r')
-                {
-                    fmt::print("Try send float.\n");
-                    
-                    std::vector<std::uint8_t> bytes;
+    	ENetAddress serverAddress;
+    	if (enet_address_set_host(&serverAddress, ENET_ADDRESS_TYPE_ANY, "localhost") != 0)
+    	{
+    		std::cout << "Failed to resolve address" << std::endl;
+    		return EXIT_FAILURE;
+    	}
+    	serverAddress.port = 10001;
 
-                    float myFloat = 32.32f;
-                    LN::SerializeArithmetic(bytes, myFloat);
+		ENetHost* host = enet_host_create(serverAddress.type, nullptr, 1, 0, 0, 0);
+		if (!host)
+		{
+			std::cerr << "Failed to create ENet host" << std::endl;
+			return EXIT_FAILURE;
+		}
+		
 
-                    // if (!clientSock.Send(bytes))
-                    // {
-                    //     fmt::print("Failed to send data to server.\n");
-                    // }
-                }
-            }
+		ENetPeer* peer = enet_host_connect(host, &serverAddress, 0, 0);
+		{
+			for (std::size_t i = 0; i < 100; ++i)
+			{
+				ENetEvent event;
+				if (enet_host_service(host, &event, 100) > 0)
+				{
+					// Nous avons un événement, la connexion a soit pu s'effectuer (ENET_EVENT_TYPE_CONNECT) soit échoué (ENET_EVENT_TYPE_DISCONNECT)
+					break; //< On sort de la boucle
+				}
+			}
 
-        }
-        
-        return EXIT_SUCCESS;
+			if (enet_peer_get_state(peer) != ENET_PEER_STATE_CONNECTED)
+			{
+				std::cout << "failed to connect" << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			// Si on arrive ici, on est connecté !
+			std::cout << "Connected!" << std::endl;
+		}
+
+		for (;;)
+		{
+			std::string message;
+			std::getline(std::cin, message);
+
+			ENetPacket* packet = enet_packet_create(message.data(), message.size(), ENET_PACKET_FLAG_RELIABLE);
+
+			enet_peer_send(peer, 0, packet);
+
+			ENetEvent event;
+			while (enet_host_service(host, &event, 1000) > 0)
+			{
+				switch (event.type)
+				{
+					case ENET_EVENT_TYPE_CONNECT:
+					{
+						std::cout << "Unexpected connect event!" << std::endl;
+						break;
+					}
+
+					case ENET_EVENT_TYPE_RECEIVE:
+					{
+						std::cout << "Received " << event.packet->dataLength << " from client #" << event.peer->incomingPeerID << ")" << std::endl;
+						std::cout << "Content: " << std::string(reinterpret_cast<const char*>(event.packet->data), event.packet->dataLength) << std::endl;
+
+						enet_packet_destroy(event.packet);
+						break;
+					}
+
+					case ENET_EVENT_TYPE_DISCONNECT:
+					{
+						std::cout << "Client #" << event.peer->incomingPeerID << " disconnected" << std::endl;
+						break;
+					}
+
+					case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT:
+					{
+						std::cout << "Client #" << event.peer->incomingPeerID << " timed out" << std::endl;
+						break;
+					}
+
+					case ENET_EVENT_TYPE_NONE:
+						std::cout << "Unexpected event none" << std::endl;
+						break;
+					}
+			}
+		}
     }
     catch (std::exception& e)
     {
--- src/Tests/Server/serverENet.cpp
+++ src/Tests/Server/serverENet.cpp
@@ -1,11 +1,11 @@
 #include <format>
+#include <iostream>
 #include <thread>
 #include <fmt/printf.h>
 #include <vector>
 #include <LittleNetwork/Serialization/Serialization.hpp>
 #include <LittleNetwork/ENet/ENetContext.hpp>
-
-#include "LittleNetwork/ENet/ENetHostEx.hpp"
+#include <enet6/enet.h>
 
 
 int main(int argc, char** argv) {
@@ -14,14 +14,60 @@ int main(int argc, char** argv) {
     {
         LN::ENetContext enetContext;
 
-        
-        while (true)
-        {
-            
+    	ENetAddress address;
+    	enet_address_build_any(&address, ENET_ADDRESS_TYPE_IPV6);
+    	address.port = 10001;
 
-        }
-        
-        return EXIT_SUCCESS;
+		ENetHost* host = enet_host_create(ENET_ADDRESS_TYPE_ANY, &address, 10, 0, 0, 0);
+		if (!host)
+		{
+			std::cerr << "Failed to create ENet host" << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		for (;;)
+		{
+			ENetEvent event;
+			while (enet_host_service(host, &event, 1000) > 0)
+			{
+				switch (event.type)
+				{
+					case ENET_EVENT_TYPE_CONNECT:
+					{
+						char address[128];
+						enet_address_get_host(&event.peer->address, address, 128);
+
+						std::cout << "A new client (#" << event.peer->incomingPeerID << ") connected from " << std::string(address) << std::endl;
+						break;
+					}
+
+					case ENET_EVENT_TYPE_RECEIVE:
+					{
+						std::cout << "Received " << event.packet->dataLength << " from client #" << event.peer->incomingPeerID << ")" << std::endl;
+						std::cout << "Content: " << std::string(reinterpret_cast<const char*>(event.packet->data), event.packet->dataLength) << std::endl;
+
+						enet_peer_send(event.peer, 0, event.packet);
+						break;
+					}
+
+					case ENET_EVENT_TYPE_DISCONNECT:
+					{
+						std::cout << "Client #" << event.peer->incomingPeerID << " disconnected" << std::endl;
+						break;
+					}
+
+					case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT:
+					{
+						std::cout << "Client #" << event.peer->incomingPeerID << " timed out" << std::endl;
+						break;
+					}
+
+					case ENET_EVENT_TYPE_NONE:
+						std::cout << "Unexpected event none" << std::endl;
+						break;
+					}
+			}
+		}
     }
     catch (std::exception& e)
     {
